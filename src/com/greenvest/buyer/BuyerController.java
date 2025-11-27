d8e30c809ad2c42e602377015edd860e91a4c56e
eshwarB-2003 <e8082409@gmail.com>
2025-11-27 13:15:55 +0000

    Buyer im progress dont change without informing !!!

--- src/com/greenvest/buyer/BuyerController.java
+++ src/com/greenvest/buyer/BuyerController.java
@@ -1,8 +1,48 @@
 package com.greenvest.buyer;
 
+import java.util.Scanner;
+
 public class BuyerController {
+    private BuyerService buyerService = new BuyerService();
+    public static void main(String[] args) {
+        new BuyerController().showMenu();
+    }
     public void showMenu(){
-        System.out.println("Welcome to the Buyer Menu");
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("=== Buyer Menu ===");
+            System.out.println("1. Login");
+            System.out.println("2. View Credits ");
+            System.out.println("3. Buy Credits ");
+            System.out.println("4. View Purchased Credits ");
+            System.out.println("0. Exit");
+            int option = scanner.nextInt();
+            switch(option){
+                case 1:
+                    System.out.println(" Username ");
+                    String username = scanner.next();
+                    System.out.println(" Password ");
+                    String password = scanner.next();
+                    buyerService.login(username, password);
+                    break;
+                case 2:
+                    buyerService.listAvailableCredits();
+                    break;
+                case 3: ;
+                    System.out.println("Enter credit ID");
+                    String creditID = scanner.next();
+                    buyerService.buyCredit(creditID);
+                    break;
+                case 4:
+                     buyerService.listPurchasedCredits();
+                     break;
+                case 0:
+                    System.out.println(" Exiting...");
+                    return;
+                default: System.out.println(" Try again, Invalid Option."); break;
+            }
+
+        }
     }
 
 }
--- src/com/greenvest/buyer/BuyerService.java
+++ src/com/greenvest/buyer/BuyerService.java
@@ -1,6 +1,78 @@
 package com.greenvest.buyer;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import com.greenvest.model.Credit;
+import com.greenvest.patterns.CreditFactory;
 
 public class BuyerService {
+    private boolean loggedIn = false;
+    public void login(String username, String password) {
+        loggedIn = true;
+        System.out.println("Buyer Logged in");
+    }
+    private boolean checkLoggedIn(){
+        if(!loggedIn){
+            System.out.println("You must be logged in to perform this actions");
+            return false;
+        }
+        return true;
+    }
+    private List<Credit> availableCredit = new ArrayList<>();
+    private List<Credit> purchasedCredit = new ArrayList<>();
 
+
+    public BuyerService() {
+        availableCredit.add(CreditFactory.createStandardCredit("CR1", 100.0, 10));
+        availableCredit.add(CreditFactory.createStandardCredit("CR2", 100.0, 15));
+        availableCredit.add(new Credit("CR3", 200.0, LocalDate.now().plusDays(20)));
+    }
+
+    public void listAvailableCredits() {
+        if(!loggedIn){
+            return; // buyer must be logged in
+        }
+        System.out.println("---- Available Credits ----");
+        for (Credit credit : availableCredit) {
+            if("ACTIVE".equals(credit.getStatus())) { // Invariant only active credits would be shown
+                System.out.println(credit.getId() + " " + credit.getAmount() + " " + credit.getExpiryDate() + " " + credit.getStatus());
+            }
+        }
+    }
+
+    public void buyCredit(String creditId) {
+        if(!loggedIn){
+            return;
+        }
+        Credit found = null;
+        for (Credit credit : availableCredit) {
+            if (credit.getId().equalsIgnoreCase(creditId)) {
+                found = credit;
+                break;
+            }
+        }
+        if (found == null) {
+            System.out.println("Credit not found");
+            return;
+        }
+        availableCredit.remove(found);
+        purchasedCredit.add(found);
+        System.out.println("Bought Credit: " + found.getId());
+    }
+
+    public void listPurchasedCredits() {
+        if(!loggedIn){
+            return;
+        }
+        System.out.println("---- Purchased Credits ----");
+        if (purchasedCredit.isEmpty()) {
+            System.out.println("Credits not bought yet");
+        } else {
+            for (Credit credit : purchasedCredit) {
+                System.out.println(credit.getId() + " " + credit.getAmount() + " " + credit.getExpiryDate());
+            }
+        }
+    }
 }
+
 
